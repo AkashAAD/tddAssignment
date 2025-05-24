f6437fd9629e68bb0eec348968985a166a9f2e9d
Akash D <deshbhratarakash@gmail.com>
2025-05-24 22:37:43 +0530

    fixed: added different delimiters input handling logic, updated specs accordingly.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,14 +1,29 @@
-require 'pry'
 class StringCalculator
   def add(str)
     return 0 if str.strip.empty?
-    raise 'Invalid input' if str.include?('\n') && str.match?(/\d,\\n/m)
-    str = str.gsub(' ', '')
 
-    str.split(',').map(&:to_i).sum
-    rescue => e
-      e.message
+    str.strip!
+    str = str.gsub("\\n", "\n")
+
+    return 'Invalid input' if str.match?(/,\n|\n,/)
+
+    delimiter = /,|\n/
+
+    if str.start_with?("//")
+      header, str = str.split("\n", 2)
+      custom_delim = header[2..]
+      delimiter = Regexp.escape(custom_delim)
+    end
+
+    numbers = str.split(/#{delimiter}/).map(&:to_i)
+    negative_numbers = numbers.select { |num| num < 0 }
+
+    if negative_numbers.any?
+      raise "Negative numbers not allowed #{negative_numbers.join(',')}"
+    end
+
+    numbers.sum
+  rescue => e
+    return e.message
   end
 end
-
-StringCalculator.new.add('1\n2,3')
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -18,13 +18,32 @@ RSpec.describe StringCalculator do
 
     context 'when input string have more than one number separated by a comma' do
       it 'returns the sum of given numbers' do
-        expect(string_calculator.add('1,5')).to eq 6
+        expect(string_calculator.add('1, 5')).to eq 6
+      end
+    end
+
+    context 'when input string have more than one number separated by a comma and \n' do
+      it 'returns the sum of given numbers' do
+        expect(string_calculator.add('1\n2,3')).to eq 6
+      end
+    end
+
+    context 'when input string have delimiters, // and \n' do
+      it 'returns the sum of the given number in the string' do
+        expect(string_calculator.add('//;\n1;2')).to eq 3
+        expect(string_calculator.add('//:\n1:3')).to eq 4
       end
     end
 
     context 'when input string is invalid' do
       it 'raise invalid input error' do
         expect(string_calculator.add('1,\n')).to eq 'Invalid input'
+      end
+    end
+
+    context 'when input string have negetive numbers' do
+      it 'raise negetive input error' do
+        expect(string_calculator.add('//;\n1;2;-8')).to eq 'Negative numbers not allowed -8'
       end
     end
   end
